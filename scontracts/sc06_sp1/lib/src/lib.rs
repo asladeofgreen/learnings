92aceab410e2bf5980118c036b4e98cbd273608b
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-09-15 17:50:54 +0200

    1. Refactoring sc06.

--- scontracts/sc06_sp1/lib/src/lib.rs
+++ scontracts/sc06_sp1/lib/src/lib.rs
@@ -3,14 +3,19 @@ const LENGTH_OF_SIGNATURE: usize = 64;
 const LENGTH_OF_VERIFIER_KEY_ED25519: usize = 32;
 const LENGTH_OF_VERIFIER_KEY_SECP256K1: usize = 33;
 
-pub type DigestBytes = [u8; LENGTH_OF_DIGEST];
-pub type SignatureBytes = [u8; LENGTH_OF_SIGNATURE];
+type DigestBytesRaw = [u8; LENGTH_OF_DIGEST];
+type SignatureBytesRaw = [u8; LENGTH_OF_SIGNATURE];
+
+pub enum DigestBytes {
+    BLAKE2B(DigestBytesRaw),
+}
+
 pub enum VerificationKeyBytes {
     ED25519([u8; LENGTH_OF_VERIFIER_KEY_ED25519]),
     SECP256K1([u8; LENGTH_OF_VERIFIER_KEY_SECP256K1]),
 }
 
-/// Verifies a BLAKE2B digest over passed data.
+/// Verifies a digest over passed data.
 ///
 /// # Arguments
 ///
@@ -18,9 +23,19 @@ pub enum VerificationKeyBytes {
 /// * `digest` - Digest to be verified.
 ///
 #[sp1_derive::cycle_tracker]
-pub fn verify_digest_blake2b(
+pub fn verify_digest(
     data: Vec<u8>,
     digest: DigestBytes
+) {
+    match digest {
+        DigestBytes::BLAKE2B(digest) => verify_digest_blake2b(data, digest),
+    }
+}
+
+#[sp1_derive::cycle_tracker]
+fn verify_digest_blake2b(
+    data: Vec<u8>,
+    digest: DigestBytesRaw
 ) {
     use blake2::{Blake2bVar, digest::{Update, VariableOutput}};
 
@@ -42,8 +57,8 @@ pub fn verify_digest_blake2b(
 ///
 #[sp1_derive::cycle_tracker]
 pub fn verify_signature(
-    digest: DigestBytes,
-    sig: SignatureBytes,
+    digest: DigestBytesRaw,
+    sig: SignatureBytesRaw,
     vk: VerificationKeyBytes
 ) {
     match vk {
@@ -52,18 +67,10 @@ pub fn verify_signature(
     }
 }
 
-/// Verifies an ED25519 ECC signature.
-///
-/// # Arguments
-///
-/// * `digest` - Digest over a message.
-/// * `sig` - Signature to be verified.
-/// * `vk` - Verification key associated over message digest signing key.
-///
 #[sp1_derive::cycle_tracker]
 fn verify_signature_ed25519(
-    digest: DigestBytes,
-    sig: SignatureBytes,
+    digest: DigestBytesRaw,
+    sig: SignatureBytesRaw,
     vk: [u8; LENGTH_OF_VERIFIER_KEY_ED25519]
 ) {
     use ed25519_consensus::{Signature, VerificationKey};
@@ -71,21 +78,14 @@ fn verify_signature_ed25519(
     let sig = Signature::try_from(sig.as_slice()).unwrap();
     let vk = VerificationKey::try_from(vk.as_slice()).unwrap();
 
+
     assert_eq!(vk.verify(&sig, &digest), Ok(()));
 }
 
-/// Verifies an SECP256K1 ECC signature.
-///
-/// # Arguments
-///
-/// * `digest` - Digest over a message.
-/// * `sig` - Signature to be verified.
-/// * `vk` - Verification key associated over message digest signing key.
-///
 #[sp1_derive::cycle_tracker]
 fn verify_signature_secp256k1(
-    digest: DigestBytes,
-    sig: SignatureBytes,
+    digest: DigestBytesRaw,
+    sig: SignatureBytesRaw,
     vk: [u8; LENGTH_OF_VERIFIER_KEY_SECP256K1]
 ) {
     use secp256k1::{ecdsa::Signature, Message, PublicKey};
--- scontracts/sc06_sp1/program/src/main.rs
+++ scontracts/sc06_sp1/program/src/main.rs
@@ -1,21 +1,27 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use sc06_lib::{verify_digest_blake2b, VerificationKeyBytes, verify_signature};
+use sc06_lib::{DigestBytes, verify_digest, VerificationKeyBytes, verify_signature};
 
 pub fn main() {
     verify_signature(
         sp1_zkvm::io::read_vec().try_into().unwrap(),
         sp1_zkvm::io::read_vec().try_into().unwrap(),
-        VerificationKeyBytes::ED25519(sp1_zkvm::io::read_vec().try_into().unwrap()),
+        VerificationKeyBytes::ED25519(
+            sp1_zkvm::io::read_vec().try_into().unwrap()
+        ),
     );
     verify_signature(
         sp1_zkvm::io::read_vec().try_into().unwrap(),
         sp1_zkvm::io::read_vec().try_into().unwrap(),
-        VerificationKeyBytes::SECP256K1(sp1_zkvm::io::read_vec().try_into().unwrap()),
+        VerificationKeyBytes::SECP256K1(
+            sp1_zkvm::io::read_vec().try_into().unwrap()
+        ),
     );
-    verify_digest_blake2b(
+    verify_digest(
         sp1_zkvm::io::read_vec(),
-        sp1_zkvm::io::read_vec().try_into().unwrap(),
+        DigestBytes::BLAKE2B(
+            sp1_zkvm::io::read_vec().try_into().unwrap()
+        ),
     );
 }
