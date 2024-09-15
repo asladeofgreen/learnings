439899d618373ce406be820adcd9e92afa45dc68
Mark A. Greenslade <asladeofgreen@gmail.com>
2024-09-15 12:23:41 +0200

    1. Refinement of basic cryptography verification functions.

--- scontracts/sc06_sp1/lib/src/lib.rs
+++ scontracts/sc06_sp1/lib/src/lib.rs
@@ -1,17 +1,43 @@
 use ed25519_consensus::{Signature as Ed25519Signature, VerificationKey as Ed25519VerificationKey};
+use secp256k1::{
+    ecdsa::{
+        RecoverableSignature as Secp256k1RecoverableSignature, RecoveryId as Secp256k1RecoveryId,
+    },
+    Message as Secp256k1Message,
+};
 
+pub type DigestBytes = Vec<u8>;
 pub type MessageBytes = Vec<u8>;
 pub type SignatureBytes = [u8; 64];
 pub type VerificationKeyBytes = [u8; 32];
 
-pub fn is_valid_signature_ed25519_1(msg: Vec<u8>, sig: Vec<u8>, vk: Vec<u8>) -> bool {
-    println!("BEGIN :: cycle-tracker :: is_valid_signature_ed25519");
+pub fn verify_digest_blake2b(data: Vec<u8>, digest: DigestBytes) -> bool {
+    println!("BEGIN :: cycle-tracker :: verify_digest_blake2b");
+
+    println!("END   :: cycle-tracker :: verify_digest_blake2b");
+
+    true
+}
+
+pub fn verify_signature_ed25519(msg: Vec<u8>, sig: Vec<u8>, vk: Vec<u8>) -> bool {
+    println!("BEGIN :: cycle-tracker :: verify_signature_ed25519");
 
     let sig = Ed25519Signature::try_from(sig.as_slice()).unwrap();
     let vk = Ed25519VerificationKey::try_from(vk.as_slice()).unwrap();
     assert_eq!(vk.verify(&sig, &msg[..]), Ok(()));
 
-    println!("END :: cycle-tracker :: is_valid_signature_ed25519");
+    println!("END   :: cycle-tracker :: verify_signature_ed25519");
+
+    true
+}
+
+pub fn verify_signature_secp256k1(msg: Vec<u8>, sig: Vec<u8>, vk: Vec<u8>) -> bool {
+    println!("BEGIN :: cycle-tracker :: verify_signature_secp256k1");
+
+    // let msg = msg[..32].try_into().unwrap();
+    // let msg = Secp256k1Message::from_digest(msg);
+
+    println!("END   :: cycle-tracker :: verify_signature_secp256k1");
 
     true
 }
--- scontracts/sc06_sp1/script/src/bin/main.rs
+++ scontracts/sc06_sp1/script/src/bin/main.rs
@@ -5,9 +5,16 @@ use sp1_sdk::{ProverClient, SP1Stdin};
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 pub const _ELF: &[u8] = include_bytes!("../../../elf/riscv32im-succinct-zkvm-elf");
 
-const MSG_ED25519_1: &str = "656432353531392d636f6e73656e7375732074657374206d657373616765";
-const SIG_ED25519_1: &str = "69261ea5df799b20fc6eeb49aa79f572c8f1e2ba88b37dff184cc55d4e3653d876419bffcc47e5343cdd5fd78121bb32f1c377a5ed505106ad37f19980218f0d";
-const VK_ED25519_1: &str = "9194c3ead03f5848111db696fe1196fbbeffc69342d51c7cf5e91c502de91eb4";
+const MSG_ED25519: &str = "656432353531392d636f6e73656e7375732074657374206d657373616765";
+const SIG_ED25519: &str = "69261ea5df799b20fc6eeb49aa79f572c8f1e2ba88b37dff184cc55d4e3653d876419bffcc47e5343cdd5fd78121bb32f1c377a5ed505106ad37f19980218f0d";
+const VK_ED25519: &str = "9194c3ead03f5848111db696fe1196fbbeffc69342d51c7cf5e91c502de91eb4";
+
+const MSG_SECP256K1: &str = "أبو يوسف يعقوب بن إسحاق الصبّاح الكندي‎";
+const SIG_SECP256K1: &str = "7b32f21b5861de8faa1c41205e6c17c6ff1167f59fe701a143cd588f1ef4833741a901c2539a7f755a7164458b1ba4acbf1ef209ce84533567a00ccfb13e3625";
+const VK_SECP256K1: &str = "0272dcc1d384a6ddad06fde1ceb2f1fe524f84ddf5ee3bdb3682eb7b927de0e682";
+
+const DIGEST_BLAKE2B: &str = "44682ea86b704fb3c65cd16f84a76b621e04bbdb3746280f25cf062220e471b4";
+const MSG_BLAKE2B: &str = "أبو يوسف يعقوب بن إسحاق الصبّاح الكندي‎";
 
 /// The arguments for the command.
 #[derive(Parser, Debug)]
@@ -40,9 +47,14 @@ fn main() {
 
 fn do_execute(args: Args) {
     let mut stdin = SP1Stdin::new();
-    stdin.write_vec(hex::decode(MSG_ED25519_1).unwrap());
-    stdin.write_vec(hex::decode(SIG_ED25519_1).unwrap());
-    stdin.write_vec(hex::decode(VK_ED25519_1).unwrap());
+    stdin.write_vec(hex::decode(MSG_ED25519).unwrap());
+    stdin.write_vec(hex::decode(SIG_ED25519).unwrap());
+    stdin.write_vec(hex::decode(VK_ED25519).unwrap());
+    stdin.write_vec(MSG_SECP256K1.as_bytes().to_vec());
+    stdin.write_vec(hex::decode(SIG_SECP256K1).unwrap());
+    stdin.write_vec(hex::decode(VK_SECP256K1).unwrap());
+    stdin.write_vec(MSG_BLAKE2B.as_bytes().to_vec());
+    stdin.write_vec(hex::decode(DIGEST_BLAKE2B).unwrap());
 
     let client = ProverClient::new();
 
